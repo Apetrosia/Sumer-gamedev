e7ad74b59145608034dd19633dbe90df642f52ab
AndreyKondakovGW <andreykondakov2302@gmail.com>
2022-08-06 10:41:24 +0300

    Update distributor script, add ability to swap weapon

--- Assets/Scripts/ECS/Controllers/DistributorComponentSystem.cs
+++ Assets/Scripts/ECS/Controllers/DistributorComponentSystem.cs
@@ -13,6 +13,7 @@ namespace Client
         readonly EcsPoolInject<InventoryComponent> inventoryPool = default; // playerTag => Inventory
         readonly EcsPoolInject<DeadTag> deadPool = default;
         readonly EcsPoolInject<Parent> parentPool = default;
+        readonly EcsPoolInject<Weapon> weaponPool = default;
 
         public void Run(IEcsSystems systems)
         {
@@ -25,12 +26,25 @@ namespace Client
                 {
                     if (!trigger.Alive(ecsWorld.Value)) continue;
                     if (!inventoryPool.Value.Has(trigger.index)) continue;
+                    if (!weaponPool.Value.Has(trigger.index)) continue;
+
                     ref var inventory = ref inventoryPool.Value.Get(trigger.index);
+                    ref var user_weapon = ref weaponPool.Value.Get(trigger.index);
+
                     inventory.items.Add(controller.item);
-                    if (!deadPool.Value.Has(entity))
+
+                    controller.item = user_weapon.value;
+
+                    ref var weapon_parent = ref parentPool.Value.Get(user_weapon.value.index);
+                    weapon_parent.value = new Entity(){index = entity,
+                        gen = ecsWorld.Value.GetEntityGen(entity)};
+
+
+                    weaponPool.Value.Del(trigger.index);
+                    /*if (!deadPool.Value.Has(entity))
                         deadPool.Value.Add(entity); // suicide
                     if (parentPool.Value.Has(controller.item.index))
-                        parentPool.Value.Del(controller.item.index);
+                        parentPool.Value.Del(controller.item.index);*/
                     break;
                 }
             }   
--- Assets/Scripts/ECS/Health/ImpactDamageSystem.cs
+++ Assets/Scripts/ECS/Health/ImpactDamageSystem.cs
@@ -13,12 +13,14 @@ namespace Client
 
         public void Run(IEcsSystems systems)
         {
+            //Снимаем все тэги получения урона
             foreach(var entity in hitFilter.Value)
             {
                 ref var health = ref healthPool.Value.Get(entity);
                 ref var impact = ref impactPool.Value.Get(entity);
                 foreach(var hit in impact.hits)
                     health.value -= hit;
+                    //Вешаем тег получн=ения урона
                 impact.hits.Clear();
             }
         }
